4e2c13eb173911d813abfe0a7c26d58ea1ab422c
Flavius Aspra <flavius.as@gmail.com>
2012-05-02 12:41:57 +0200

    exception handling

--- module/Guestbook/src/Guestbook/Model/EntryProvider.php
+++ module/Guestbook/src/Guestbook/Model/EntryProvider.php
@@ -35,13 +35,15 @@ class EntryProvider implements \Iterator, \Zend\Loader\LocatorAware {
         $locator = $this->getLocator();
         try {
             //TODO howto PDO::setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION) ?
-            $this->dbconnection = $this->getLocator()->get('masterdb');
-            $this->dbconnection->setAttribute(\PDO::ATTR_ERRMODE, \PDO::ERRMODE_EXCEPTION);
-            $this->stmt = $this->dbconnection->query('SELECT gb_entry_id as id, text, date FROM entries');//TODO LIMIT, order by, etc
+            $this->dbconnection = @$this->getLocator()->get('masterdb');
+            var_dump($this->dbconnection->errorCode());
+            //$this->dbconnection->setAttribute(\PDO::ATTR_ERRMODE, \PDO::ERRMODE_EXCEPTION);
+            $this->stmt = $this->dbconnection->query('SELECT gb_entry_id as id, text, date FROM entries_');//TODO LIMIT, order by, etc
             $this->row = $this->stmt->fetch(\PDO::FETCH_ASSOC, \PDO::FETCH_ORI_REL, 0);
         }
         catch(\PDOException $e) {
             $this->row = array();
+            var_dump($this->dbconnection->errorCode());
             //TODO error logging
         }
         $this->pos = 0;
